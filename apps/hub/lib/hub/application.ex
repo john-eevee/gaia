0f90ff05d26dad4f1f5fe4d8595b9315b08c0ffc
Evangelista <5256711+john-eevee@users.noreply.github.com>
2025-11-02 01:59:00 -0300

    chore: init app hub

---
+++ apps/hub/lib/hub/application.ex
@@ -0,0 +1,20 @@
+defmodule Hub.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Hub.Worker.start_link(arg)
+      # {Hub.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Hub.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
