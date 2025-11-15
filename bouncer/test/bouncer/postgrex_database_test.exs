469db968f7955f4382ec1e1832ea3e88e2eb4491
John <5256711+john-eevee@users.noreply.github.com>
2025-11-15 16:26:27 -0300

    fix(bouncer): correct parameter type in test case
    Updated the parameter type in the PostgrexDatabase test to ensure it matches the expected string format.

--- bouncer/test/bouncer/postgrex_database_test.exs
+++ bouncer/test/bouncer/postgrex_database_test.exs
@@ -6,10 +6,10 @@ defmodule Bouncer.PostgrexDatabaseTest do
   @tag :ci
   test "should pass the query and params to postgrex functions" do
     sql = "SELECT $1 "
-    params = [42]
+    params = ["42"]
 
     # We can't easily mock Postgrex here without significant setup,
     # so we'll just ensure that the function can be called without error.
-    assert {:ok, %{rows: [[42]]}} = PostgrexDatabase.query(sql, params)
+    assert {:ok, %{rows: [["42"]]}} = PostgrexDatabase.query(sql, params)
   end
 end
