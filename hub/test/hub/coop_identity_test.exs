6a665d8af184296a32406e6d7570205d48a5e3a2
John <5256711+john-eevee@users.noreply.github.com>
2025-11-19 14:56:49 -0300

    refactor(hub): improve farm member test for consistency
    Replaced alias for Changesets and updated test cases to use valid_farm_member_attrs for better clarity and consistency.

--- hub/test/hub/coop_identity_test.exs
+++ hub/test/hub/coop_identity_test.exs
@@ -1,6 +1,7 @@
 defmodule Gaia.Hub.CoopIdentityTest do
   use ExUnit.Case, async: true
-  alias Gaia.Hub.CoopIdentity.FarmMemberFixtures
+  alias Gaia.TestingFacility.Changesets
+  import Gaia.Hub.CoopIdentity.FarmMemberFixtures
 
   setup tags do
     pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Gaia.Hub.Repo, shared: not tags[:async])
@@ -10,9 +11,15 @@ defmodule Gaia.Hub.CoopIdentityTest do
 
   describe "register_farm/1" do
     test "should insert a new farm when the attributes are valid" do
-      attrs = FarmMemberFixtures.valid_farm_member_attrs()
+      attrs = valid_farm_member_attrs()
       assert {:ok, farm_member} = Gaia.Hub.CoopIdentity.register_farm(attrs)
       assert farm_member.id != nil
+    end
+
+    test "should return a changeset with errors when attributes are invalid" do
+      attrs = valid_farm_member_attrs() |> Map.delete(:name)
+      assert {:error, changeset} = Gaia.Hub.CoopIdentity.register_farm(attrs)
+      assert %{name: ["can't be blank"]} = Changesets.errors_on(changeset)
     end
   end
 end
