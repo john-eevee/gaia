7918d99f13d97d6b80008a9cfa9a02470154aed5
John <5256711+john-eevee@users.noreply.github.com>
2025-12-04 12:04:02 -0300

    feat(hub): add migration for farmers table and role type
    Create a new migration to define the farmers table with fields for id, email, first name, last name, role, and farm member reference. Introduce a custom enum type for farmer roles.

---
+++ src/hub/priv/repo/migrations/20251204144501_create_farmer.exs
@@ -0,0 +1,32 @@
+defmodule Gaia.Hub.Repo.Migrations.CreateFarmer do
+  use Ecto.Migration
+
+  def up do
+    execute("CREATE TYPE farmer_role AS ENUM ('owner', 'staff', 'admin');")
+
+    create table(:farmers, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :email, :string, null: false
+      add :first_name, :string, null: false
+      add :last_name, :string, null: false
+      add :role, :farmer_role, null: false
+
+      add :farm_member_id,
+          references(
+            :farm_members,
+            type: :binary_id,
+            on_delete: :delete_all
+          ), null: false
+
+      timestamps()
+    end
+
+    create unique_index(:farmers, [:email])
+  end
+
+
+  def down do
+    drop table(:farmers)
+    execute("DROP TYPE farmer_role;")
+  end
+end
