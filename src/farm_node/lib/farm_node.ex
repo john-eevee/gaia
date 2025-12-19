2e4e228746d17fbab59f95a83df8f4d299558be9
John <5256711+john-eevee@users.noreply.github.com>
2025-12-19 16:59:28 -0300

    chore(app): add FarmNode application entry
    Add main module and Application supervisor skeleton for the Farm Node app.

---
+++ src/farm_node/lib/farm_node.ex
@@ -0,0 +1,18 @@
+defmodule Gaia.FarmNode do
+  @moduledoc """
+  Documentation for `FarmNode`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gaia.FarmNode.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
