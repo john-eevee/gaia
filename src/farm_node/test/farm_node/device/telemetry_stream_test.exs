d7fa1b1a74ff2b7acda3c8b0ef8344c8263382cf
John <5256711+john-eevee@users.noreply.github.com>
2025-12-20 01:08:50 -0300

    test: add unsubscribe behavior to telemetry stream tests

--- src/farm_node/test/farm_node/device/telemetry_stream_test.exs
+++ src/farm_node/test/farm_node/device/telemetry_stream_test.exs
@@ -10,6 +10,14 @@ defmodule Gaia.FarmNode.Device.TelemetryStreamTest do
     assert_receive {:telemetry, "test_topic", %{foo: :bar}}, 500
   end
 
+  test "unsubscribe should not receive messages after unsubscribing" do
+    {:ok, _} = subscribe_to_test_topic()
+    :ok = TelemetryStream.unsubscribe("test_topic")
+    TelemetryStream.broadcast("test_topic", %{foo: :bar})
+
+    refute_receive {:telemetry, "test_topic", %{foo: :bar}}, 500
+  end
+
   defp subscribe_to_test_topic do
     # Ensure application is running so Registry is available
     Application.ensure_all_started(:farm_node)
