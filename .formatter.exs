9a71caaf888a9a388b4e1f96fe45bac909cfe7fb
John <5256711+john-eevee@users.noreply.github.com>
2026-01-14 20:40:46 -0300

    change subdirectory

--- .formatter.exs
+++ .formatter.exs
@@ -1,7 +1,5 @@
 # Used by "mix format"
 [
   inputs: ["{mix,.formatter,.workspace}.exs", "config/*.exs"],
-  # by default packages are expected under a packages directory but you
-  # can add any other subdirectory
-  subdirectories: ["packages/*"]
+  subdirectories: ["src/*"]
 ]
