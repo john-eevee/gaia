5c510f14d1a958969516fc57eea55ed396f81159
John <5256711+john-eevee@users.noreply.github.com>
2026-01-15 14:04:56 -0300

    fix workspace paths, remove log from common mix

--- .workspace.exs
+++ .workspace.exs
@@ -2,7 +2,7 @@
   # Add paths that shall not be considered when generating the workspace graph
   # By default deps and _build folders are ignored. You are advised to store all
   # build artifacts under a dedicated folder (e.g. artifacts) which should be ignored.
-  ignore_paths: ["_build", "deps", "adr", "src/build"],
+  ignore_paths: ["_build", "deps", "adr", "src/build_common"],
 
   # Custom workspace checks for linting your mono-repo at a package level. You can
   # enforce things like common build dirs or required project depenendencies. For
